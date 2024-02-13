17deae7199dc4855415a69038bdca0c0d8fa174d
Ravi Kiran Srikakaolapu <ravikiransrikakaolapu@Ravis-MacBook-Air.local>
2024-02-13 10:05:20 +0530

    added components

--- src/app/cr-form/cr-form.module.ts
+++ src/app/cr-form/cr-form.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CrFormComponent } from './cr-form.component';
 import { CrFormRoutingModule } from './cr-form-routing.module';
+import { CrLibModule } from 'node_modules/cr-lib';
 
 const EXPORTS = [CrFormComponent];
 @NgModule({
   declarations: [...EXPORTS ],
   imports: [
     CommonModule,
-    CrFormRoutingModule
+    CrFormRoutingModule,
+    CrLibModule
   ],
   exports: [...EXPORTS]
 })
